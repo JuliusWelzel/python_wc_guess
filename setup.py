b14f61dac5f0f2b465fb26e577a134317d84dc90
Julius Welzel <julius.welzel@gmail.com>
2023-07-21 10:21:00 +0200

    [FIX] update `setup.py` with all packages

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     author='Julius Welzel',
     packages=find_packages(),
     install_requires=[
+        'numpy>=1.21.6'
         'pandas>=1.3.0',
+        'scikit-learn>=1.2.2'
         'statsmodels>=0.13.0',
         'rapidfuzz>=1.5.1',
     ],
